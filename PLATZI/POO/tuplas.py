c3868965576d7865a16fd96ee4a6bd0d7f732d24
EdwinSanFI <jaret-@live.com.mx>
2021-05-17 13:07:18 -0500

    Cambiando de lugar el archivo

--- PLATZI/POO/tuplas.py
+++
@@ -1,5 +0,0 @@
-tuplas = ('Loki', 'Duke', 'Princesa', 'Lisa')
-primer_elemento, segundo_elemento, tercer_elemento, cuarto_elemento = tuplas
-print(primer_elemento)
-print(segundo_elemento)
-print(cuarto_elemento)
